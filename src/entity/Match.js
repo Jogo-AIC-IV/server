64934de6b08dbf51ee5b7dcffa1a6ac5f62fc86f
Nicolas Bortoluzzi <bortoluzzinicolas@gmail.com>
2021-07-31 16:03:10 -0300

    update match

--- src/entity/Match.js
+++ src/entity/Match.js
@@ -1,15 +1,47 @@
 function createMatch(firstPlayer, secondPlayer) {
 
-    delete firstPlayer['socket'];
-    delete secondPlayer['socket'];
-
-    return {
+    const match = {
         id: Math.floor(Math.random() * 1000),
-        state: {
-            firstPlayer,
-            secondPlayer
+        state: {},
+
+        runTurn: function() {
+            
         }
     }
+
+    match.state[firstPlayer.socket.id] = {
+        name: firstPlayer.name,
+        life: 3,
+        price: 100,
+        money: 350,
+        enemies: {
+            count: 0,
+            first: 0,
+            list: []
+        },
+        towers: {
+            list: [],
+            types: []
+        }
+    }
+
+    match.state[secondPlayer.socket.id] = {
+        name: secondPlayer.name,
+        life: 3,
+        price: 100,
+        money: 350,
+        enemies: {
+            count: 0,
+            first: 0,
+            list: []
+        },
+        towers: {
+            list: [],
+            types: []
+        }
+    }
+
+    return match;
 }
 
 module.exports = createMatch 
